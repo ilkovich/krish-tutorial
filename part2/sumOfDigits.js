50d14c7fef2476b713fab060b15770c192290967
Daniel Ilkovich <daniel+github@betaworks.com>
2015-05-18 15:49:25 -0400

    sumOfDigits solutions

--- part2/sumOfDigits.js
+++ part2/sumOfDigits.js
@@ -1,4 +1,13 @@
 function sumOfDigits(num) {
+    num=num+"";
+
+    if(!num) return 0;
+    
+    var test = parseInt(num[0])
+      , rem  = num.substr(1)
+    ; 
+
+    return (isNaN(test) ? 0 : test) + sumOfDigits(rem);
 }
 
 module.exports = sumOfDigits;
